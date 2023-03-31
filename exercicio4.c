4304d739070cadd894b18c897d08bb715aec6d8e
Pedrohome <129235102+Pedrohome@users.noreply.github.com>
2023-03-30 21:22:49 -0300

    novo exercicio

---
+++ exercicio4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int num_elementos, i;
+    float num, soma = 0.0, media;
+
+    printf("Quantos numeros voce deseja inserir?");
+    scanf("%d", &num_elementos);
+
+    for (i = 1; i <= num_elementos; ++i){
+        printf("Digite o numero %d: ", i);
+        scanf("%f", &num);
+        soma += num;
+    }
+
+    media = soma / num_elementos;
+    printf("A média dos numeros digitados é; %.2f", media);
+
+    return 0
+}
