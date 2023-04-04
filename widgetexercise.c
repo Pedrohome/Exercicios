774d4de55dcf842aedbb7b0df9d9931160093e91
Pedrohome <129235102+Pedrohome@users.noreply.github.com>
2023-04-03 23:02:01 -0300

    02

--- widgetexercise.c
+++ widgetexercise.c
@@ -1,17 +1,6 @@
-#include <gtk/gtk.h>
-
-typedef struct 
-{
-    GtkWidget parent;
-    gchar *text;
-} CustomLabel;
-
-typedef struct
-{
-    GtkWidgetClass parent_class;
-} CustomLabelClass;
+#include <stdio.h>
 
 
-#define CUSTOM_TYPE_LABEL (custom_label_get_type())
+
 
 
